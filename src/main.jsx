1f9048a6cc63340cb757cdd4083a88f0661ebbc8
h.fukuda <pgyoga.memberwork@gmail.com>
2026-05-18 16:20:57 +0900

    feat: 開発環境とGitHub Pagesの両方でルーティングが正しく動作するように修正

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pgyoga-learning">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
